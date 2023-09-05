d288eeca269c2cad25e18b18f06b03b5bbf92635
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2023-09-06 00:18:14 +0600

    mysql package, controller, router added to the api

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const helmet = require("helmet");
+const apiRouter  = require('./routes/apiRoutes');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
+
 // defining the Express app
 const app = express();
-
+app.use(express.json());
 app.use(helmet());
 
 // using bodyParser to parse JSON bodies into JS objects
@@ -17,9 +19,10 @@ app.use(bodyParser.json());
 // enabling CORS for all requests
 app.use(cors());
 
-
 // adding morgan to log HTTP requests
 app.use(morgan('combined'));
+
+app.use('/api/v1', apiRouter);
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
